107790baf6902dd9173c3cd1a2f2df4c3b330dd9
kesarai <123195247+kesarai@users.noreply.github.com>
2023-05-10 18:37:02 +0100

    game complete, lost commitments due to lost branch after pc restart

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function playGame(playerChoice){
-  let result = '';
+let win = 0;
+let lose = 0;
+let draw = 0;
+result = '';
+    
+function oneRound(playerChoice){
   let computerChoice = getComputerChoice();
   console.log(playerChoice);
   console.log(computerChoice);
@@ -21,33 +25,75 @@ function playGame(playerChoice){
   } else {
     result = "Check your spelling!";
   }
-
-  alert(result);
+  document.getElementById("result").textContent = result;
 }
 
 function getComputerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
-  return choices[Math.floor(Math.random() * choices.length)].toLowerCase();
-  
+  return choices[Math.floor(Math.random() * choices.length)].toLowerCase()
+}
+function disableButtons() {
+  let buttons = document.getElementsByClassName('btn');
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].disabled = true;
+  }
+}
+function enableButtons() {
+  let buttons = document.getElementsByClassName('btn');
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].disabled = false;
+  }
+}
+
+function resetGame() {
+  win = 0;
+  lose = 0;
+  draw = 0;
+  result = '';
+  alert('Game reset');
+  document.getElementById("win").textContent = win;
+  document.getElementById("lose").textContent = lose;
+  document.getElementById("result").textContent = '';
+  document.getElementById("finalResult").textContent = '';
+  enableButtons();
 }
 
 
-function getPlayerChoice(){
+function playGame(){
   let playerChoice = '';
   let buttons = document.getElementsByClassName('btn');
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function(e) {
       playerChoice = e.target.textContent.toLowerCase();
-      playGame(playerChoice); 
-      ;
-    })
+      oneRound(playerChoice);
+      if (result === "You win!") {
+        ++win;
+        document.getElementById("win").textContent = win;
+        document.getElementById("lose").textContent = lose;
+      } else if (result === "You lose!") {
+        ++lose;
+        document.getElementById("win").textContent = win;
+        document.getElementById("lose").textContent = lose;
+      } else {
+        ++draw;
+        document.getElementById("win").textContent = win;
+        document.getElementById("lose").textContent = lose;
+      }
+      if (win === 5) {
+        disableButtons();
+        document.getElementById("finalResult").textContent = "Congratulations! You defeated the PC.";
+        setTimeout(() => { resetGame(); }, 3000);
+      } else if (lose === 5) {
+        disableButtons();
+        document.getElementById("finalResult").textContent = "Defeated! You couldn't handle the PC.";
+        setTimeout(() => { resetGame(); }, 3000);
+      }
+    });
   }
 }
 
-
-
-getPlayerChoice();
-      
+playGame();
+     
   
       
 
